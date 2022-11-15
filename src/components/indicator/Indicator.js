5af38d6fcbe6af5fbd2a2fc0ef6d55086a2f90fb
Aaryan121 <aaryan.cs01@gmail.com>
2022-11-15 19:27:54 +0545

    skills component completed

--- src/components/indicator/Indicator.js
+++ src/components/indicator/Indicator.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import "./Indicator.css"
+import Img1 from "../../assets/img/fillhex.svg"
+import Img2 from "../../assets/img/emptyhex.svg"
 
-function Indicator() {
-    const data = 3;
+function Indicator(props) {
+    const data = props.level;
+    const arr = []
+
+    for (let i=0 ; i<data ; i++){
+        arr.push("img1");
+    }
+    for (let i=0; i< (5-data); i++){
+        arr.push("img2")
+    }
+    console.log(arr)
   return (
-    <div>
-        <div className="title">HTML</div>
-
+    <div className='skillWarper'>
+        <div className="skilltitle">{props.skill}</div>
+        <div className="hexa">
+            {arr.map((e) =>  {
+                if(e == "img1"){
+                    return <img src={Img1} alt="" />
+                }
+                else{
+                    return <img src={Img2} alt="" />
+                }
+            } )}
+        </div>
     </div>
   )
 }
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,4 +1,6 @@
+import Indicator from "../indicator/Indicator"
 import "./Skills.css"
+
 
 const Skills = () => {
   return (
@@ -7,7 +9,21 @@ const Skills = () => {
             My Skills
         </div>
         <div className="skillsWarper">
-            
+            <Indicator level="4" skill="HTML" />
+            <Indicator level="3" skill="CSS" />
+            <Indicator level="3" skill="JavaScript" />
+            <Indicator level="3" skill="React" />
+            <Indicator level="3" skill="NodeJs" />
+            <Indicator level="2" skill="MongoDB" />
+            <Indicator level="2" skill="MySQL" />
+        </div>
+        <div className="expl">
+          <ul>
+            <li>Integrating apis to frontend applications.</li>
+            <li>Develop interactive User Interfaces for your web applications.</li>
+            <li>Using modern technologies for building applications.</li>
+            <li>Developing UI/UX as per the need.</li>
+          </ul>
         </div>
     </div>
   )
