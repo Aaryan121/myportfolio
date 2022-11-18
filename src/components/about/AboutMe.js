26003f83af9121f59d0f166c5ebd03093fdaa1dc
Aaryan121 <aaryan.cs01@gmail.com>
2022-11-18 17:50:55 +0545

    commit

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -11,7 +11,9 @@ const AboutMe = () => {
             </div>
             <div className="rightabout">
                 <p className="aboutdesc">
-                Currently a student pursuing bachelors in Computer Science and Information Technology. I'm passionate about web development and programming in general. I like to learn new emerging technologies and showcase my skills.
+                Currently a Student pursuing BSc CSIT on ASCOL, I usually like to learn about new things in pogramming world, web development in general. 
+                I have done several courses on React Js and few projects also based on MERN stack. 
+                I can learn things quickly, and have experience to adapt with new teams quicker. 
                 </p>
             </div>
         </div>
